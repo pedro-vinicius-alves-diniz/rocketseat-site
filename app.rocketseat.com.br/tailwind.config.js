754b1ce206c8a6ec3ade7ba66ed7f144e9686118
Pedro Vinicius Alves Diniz <104076903+pedro-vinicius-alves-diniz@users.noreply.github.com>
2024-12-28 20:48:37 +0100

    Construção parte visual topbar

--- app.rocketseat.com.br/tailwind.config.js
+++ app.rocketseat.com.br/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         'dark-1': "#121214",
         'dark-2': '#1A1A1E',
-        'primary-1': '#8257E5',
+        'primary-1': 'rgb(130, 52, 233)',
         'primary-2': '#9956F6'
       }
     },
