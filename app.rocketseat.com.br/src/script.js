9aacf7726e4a365acd616b52537b11835fb7499e
Pedro Vinicius Alves Diniz <104076903+pedro-vinicius-alves-diniz@users.noreply.github.com>
2024-12-31 00:22:18 +0100

    Animação Icon-Menu

--- app.rocketseat.com.br/src/script.js
+++ app.rocketseat.com.br/src/script.js
@@ -1,4 +1,27 @@
 const contentPage = document.getElementById("content-page");
 const menuContent = document.getElementById("icon-menu-content");
 const menuItem = document.getElementById("icon-menu-item");
-const optionsSidebar = document.querySelectorAll(".")
+const optionsSidebar = document.querySelectorAll(".item-sidebar");
+let menuStatus = "Expandido";
+
+menuContent.addEventListener("click", () => {
+  console.log("Botão menu clicado!");
+
+  if (menuStatus === "Expandido") {
+    console.log("Restringido");
+    menuStatus = "Restringido";
+
+    menuItem.src = "assets/icons/icon-menu-right.svg";
+    optionsSidebar.forEach((elemento) => {
+      elemento.classList.add("hidden");
+    });
+  } else {
+    console.log("Expandido");
+    menuStatus = "Expandido";
+
+    menuItem.src = "assets/icons/icon-menu-left.svg";
+    optionsSidebar.forEach((elemento) => {
+      elemento.classList.remove("hidden");
+    });
+  }
+});
