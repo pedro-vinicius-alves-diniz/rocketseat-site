3d2d3e5aa6169a72bfc6bda19d56238fe3312a62
Pedro Vinicius Alves Diniz <104076903+pedro-vinicius-alves-diniz@users.noreply.github.com>
2024-12-31 01:37:08 +0100

    update

--- app.rocketseat.com.br/tailwind.config.js
+++ app.rocketseat.com.br/tailwind.config.js
@@ -4,16 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-1': "#121214",
-        'dark-2': '#1A1A1E',
-        'primary-1': 'rgb(130, 52, 233)',
-        'primary-2': '#9956F6'
+        "dark-1": "#121214",
+        "dark-2": "#1A1A1E",
+        "primary-1": "rgb(130, 52, 233)",
+        "primary-2": "#9956F6",
       },
       backgroundImage: {
-        'banner': "url('./assets/background/retrospective-desktop.png')"
-      }
+        'banner': "url('./assets/background/retrospective-desktop.png')",
+      },
     },
   },
   plugins: [],
-}
-
+};
